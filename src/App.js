9e9b325bf8767c006dd03a14155fad6d89009826
sakshi-1 <sakshis.ug18.ec@nitp.ac.in>
2020-08-05 11:45:09 +0530

    added redux and implemented userReducer and userActions to the header and app component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom';
+
+import {connect} from 'react-redux';
+
+import {setCurrentUser} from './redux/user/user.actions';
 
 import HomePage from './pages/homepage/homepage.component';
 
@@ -15,33 +19,30 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import './App.css';
 
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state={
-      currentUser: null
-    }
-  }
-
+  
   unsubscribeFromAuth=null
 
   componentDidMount() {
+    const {setCurrentUser} = this.props;
+
+
     this.unsubscribeFromAuth=auth.onAuthStateChanged(async userAuth=>{
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot=>{
-          this.setState({
-            currentUser: {
-              id : snapShot.id,
+          setCurrentUser({
+            
+              id: snapShot.id,
               ...snapShot.data()
-            }
+            
 
-          }
-          )
+          
+           } );
         });
       }
       else{
-        this.setState({currentUser: userAuth});
+        setCurrentUser(userAuth);
       }
      
     });
@@ -55,7 +56,7 @@ class App extends React.Component {
 render(){
   return (
     <div>
-      <Header currentUser={this.state.currentUser}/>
+      <Header />
       <Switch>
       <Route exact path='/' component={HomePage}/>
       <Route path='/shop' component={ShopPage}/>
@@ -72,4 +73,8 @@ render(){
   
 }
 
-export default App;
+const mapDispatchToProps= dispatch=>({
+  setCurrentUser: user=> dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import './index.css';
+import {Provider} from 'react-redux';
 import App from './App';
+import store from './redux/store';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
   <App />
-  </BrowserRouter>,
+  </BrowserRouter>
     
+
+  </Provider>,
+  
   
   document.getElementById('root')
 );
