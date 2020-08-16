dd37746dbfb89a482faff879e177d254e44880d9
sakshi-1 <sakshis.ug18.ec@nitp.ac.in>
2020-08-16 11:42:48 +0530

    mived the directory and shop data into it's own reducer

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import {Provider} from 'react-redux';
 import App from './App';
-import store from './redux/store';
+import {store, persistor} from './redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-  <App />
+    <PersistGate persistor={persistor}>
+    <App />
+
+    </PersistGate>
+  
   </BrowserRouter>
     
 
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,33 +1,24 @@
 import React from 'react';
-import SHOP_DATA from './shop.data.js';
+import { Route } from 'react-router-dom';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionPage from '../../pages/collection/collection.component';
+
+import CollectionOverview from '../collection/collection.component';
 
 
 
-class ShopPage extends React.Component{
-    constructor(props){
-        super(props);
+const ShopPage = ({match})=>(
+<div className='shop-page'>
+<Route exact path={`${match.path}`} component={
+        CollectionOverview}/>
+<Route path={`${match.path}/:collectionId`} component={CollectionPage} />
 
-        this.state={
-            collections:SHOP_DATA
-              
-              
-              
-        }
-    }
-    render(){
-        const {collections}=this.state;
-        return(<div className='shop-page'>
-            {
-                collections.map(({id,...otherCollectionProp}) =>(
-                    <CollectionPreview key={id} {...otherCollectionProp}/>
-                )
-                    )
-            }
         </div>
 
-        );
-    }
-}
+        
+    
+);
+
+
+
 export default ShopPage;
